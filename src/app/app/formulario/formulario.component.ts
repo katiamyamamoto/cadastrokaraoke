5296417c0250f1c9030f371fe8354c6a4e35c721
Katia Yamamoto <mayumee@gmail.com>
2025-05-27 12:24:11 -0300

    Ajustes finais

--- src/app/app/formulario/formulario.component.ts
+++ src/app/app/formulario/formulario.component.ts
@@ -1,11 +1,27 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [FormsModule, HttpClientModule ],
+  providers: [HttpClient],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  public name: string = "";
+  public cpf: string = "";
+  public dob: Date = new Date();
+  public phone: string = "";
+  public email: string = "";
+  constructor(private http: HttpClient){
+  }
 
+  public Send(): void{
+    const cadastro = {name: this.name, cpf : this.cpf, dob : this.dob, phone: this.phone, email: this.email};
+    this.http.post("https://localhost:7006/Cadastro", cadastro).subscribe(() => {
+      alert("Cadastro realizado com sucesso, Estamos aguardando voce!");
+    })
+  }
 }
